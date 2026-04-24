18ba651f13cba07c67eaa281d98a81ac396ee422
Juan Jose <juanjo5792@gmail.com>
2026-04-24 18:33:16 -0500

    Update index.vue to enhance layout and styling of the main content

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,7 +3,10 @@
     <Title>Robótica</Title>
   </Head>
 
-  <h1>Hello there</h1>
+  <main class="text-secondary h-screen flex flex-col items-center justify-center gap-4">
 
-  <NuxtLink to="/dashboard">Go to dashboard</NuxtLink>
+    <h1 class="text-2xl font-bold">Hello there, no cacorros allowed</h1>
+    <NuxtLink to="/dashboard" class="underline hover:text-white transition-colors duration-500">Go to dashboard</NuxtLink>
+  </main>
+
 </template>
